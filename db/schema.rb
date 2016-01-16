9adbad845c48e8fa1ddf0421b2ce5e0aaebd41c1
Jordan Hudgens <jordan@wow.com>
2016-01-16 12:09:34 -0600

    Integrated the resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160116180725) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.string   "director"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
